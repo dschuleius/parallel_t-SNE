bbf4f6de5245b4020d0b3a6d5777425b83f72790
Julius Kobialka <J.Kobialka@campus.lmu.de>
2023-01-17 18:16:33 +0100

    SparkImplementation pairwiseDistance working on RDD.

--- build.sbt
+++ build.sbt
@@ -2,12 +2,15 @@ ThisBuild / version := "0.1.0-SNAPSHOT"
 
 ThisBuild / scalaVersion := "2.13.10"
 
+
+// breeze is disabled because spark-mllib also imports breeze but a different version
+// so that the program throws an error at compile time.
 lazy val root = (project in file("."))
   .settings(
     name := "parallel_t-SNE",
     libraryDependencies += "org.apache.spark" %% "spark-core" % "3.3.1",
     libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.3.1" ,
     libraryDependencies += "org.apache.spark" %% "spark-hive" % "3.3.1",
-    libraryDependencies += "org.scalanlp" %% "breeze" % "2.1.0",
+    // libraryDependencies += "org.scalanlp" %% "breeze" % "2.1.0",
     libraryDependencies += "org.apache.spark" %% "spark-mllib" % "3.3.1"
   )
--- src/main/scala/SparkImplementation.scala
+++ src/main/scala/SparkImplementation.scala
@@ -8,24 +8,31 @@ import org.apache.spark.storage.StorageLevel
 
 import scala.io.Source
 import breeze.linalg._
+import breeze.storage._
 import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.mllib.linalg.distributed.RowMatrix
 import org.apache.spark.rdd.RDD
 
 object SparkImplementation extends App{
 
-  val conf = new SparkConf().setAppName("distributed_t-SNE").setMaster("local[1]").set("spark.driver.host", "127.0.0.1").set("spark.driver.bindAddress", "127.0.0.1")
+  // set up Spark, changing to local host.
+  val conf = new SparkConf()
+    .setAppName("distributed_t-SNE")
+    .setMaster("local[1]")
+    .set("spark.driver.host", "127.0.0.1")
+    .set("spark.driver.bindAddress", "127.0.0.1")
   val sc = new SparkContext(conf)
 
+  // function that imports MNIST from .txt files.
   def importData(fileName: String, sampleSize: Int): Array[Array[Double]] = {
-    // Read the file and split it into lines
+    // read the file and split it into lines
     val lines = Source.fromFile(fileName).getLines.take(sampleSize).toArray
 
-    // Split each line into fields and convert the fields to doubles
+    // split each line into fields and convert the fields to doubles
     // trim removes leading and trailing blank space from each field
     val data = lines.map(_.trim.split("\\s+").map(_.toDouble))
 
-    // Return the data as an array of arrays of doubles
+    // return the data as an array of arrays of doubles
     data
   }
 
@@ -33,6 +40,51 @@ object SparkImplementation extends App{
   // relative path does not work, probably problem with SBT folder structure
   val MNISTlabels = sc.parallelize(importData("/Users/juli/Documents/WiSe_2223_UniBo/ScalableCloudProg/parralel_t-SNE/data/mnist2500_labels.txt", 10))
   val MNISTdata = sc.parallelize(importData("/Users/juli/Documents/WiSe_2223_UniBo/ScalableCloudProg/parralel_t-SNE/data/mnist2500_X.txt", 10))
+
+  // testing
   MNISTdata.take(10).foreach(println)
 
+  def euclideanDistance(point1: Array[Double], point2: Array[Double]): Double = {
+    // Calculate the squared Euclidean distance between the two points
+    val squaredDistance = point1.zip(point2).map { case (x, y) => (x - y) * (x - y) }.sum
+
+    // Return the Euclidean distance
+    Math.sqrt(squaredDistance)
+  }
+
+  // cartesian function very expensive on big data sets, as new RDD with all possible combinations is created.
+  def pairwiseDistances(points: RDD[Array[Double]]): RDD[((Int, Int), Double)] = {
+    val indexedPoints = points.zipWithIndex().map { case (point, index) => (index, point) }
+    val pointPairs = indexedPoints.cartesian(indexedPoints)
+    pointPairs.map { case ((index1, point1), (index2, point2)) =>
+      ((index1.toInt, index2.toInt), euclideanDistance(point1, point2))
+    }
+  }
+
+  pairwiseDistances(MNISTdata).take(5).foreach(println)
+
+  def computeSimilarityScoresGauss(distances: RDD[((Int, Int), Double)], sigma: Double): RDD[((Int, Int), Double)] = {
+
+    val n = distances.count().toInt
+    val unnormSimilarities = distances.map { case ((i, j), d) =>
+      ((i, j), math.exp(-1 * scala.math.pow(d, 2) / (2 * scala.math.pow(sigma, 2))))
+    }
+
+    val denominators = unnormSimilarities.map { case ((i, _), _) => i }.distinct().map { i =>
+      (i, unnormSimilarities.filter { case ((k, _), _) => k != i }.map { case (_, d) =>
+        math.exp(-1 * scala.math.pow(d, 2) / (2 * scala.math.pow(sigma, 2)))
+      }.sum())}.map { case (ind, sim) => ((ind, 0), sim)}
+
+    val unnormSimilaritiesWithDenominator = unnormSimilarities.join(denominators).map { case ((i, j), (unnorm, denominator)) =>
+      ((i, j), unnorm / denominator) }
+
+    val normSimilarities = unnormSimilaritiesWithDenominator.map { case ((i, j), s) =>
+      ((i, j), (s + unnormSimilaritiesWithDenominator.lookup((j, i)).head) / (2 * n))
+    }
+    normSimilarities
+  }
+
+
+  // testing computeSimilarityScoreGauss
+  computeSimilarityScoresGauss(pairwiseDistances(MNISTdata), sigma = 1).take(10).foreach(println)
 }
