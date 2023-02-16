d9c7dc9d88a503ed1c8edf649daf5ec705196fe2
schererant <67601986+schererant@users.noreply.github.com>
2023-02-16 17:33:25 +0000

    Bug fix bash script #3

--- deploy_dataproc.sh
+++ deploy_dataproc.sh
@@ -3,7 +3,7 @@
 
 # Use yq to get values from config.yaml
 getYamlValue() {
-  yq ".shellConfig.$1" src/main/resources/config.yaml
+  yq "$1" src/main/resources/config.yaml
 }
 
 # Package the application
@@ -11,63 +11,66 @@ sbt clean package
 
 # Specify core/project
 gcloud config set core/project \
-  "$(getYamlValue gcProjectName)"
+  "$(getYamlValue .shellConfig.gcProjectName)"
 
 # Copy the jar to the server
 #gsutil cp target/scala-2.12/parallel_t-SNE-assembly-0.1.0-SNAPSHOT.jar gs://scala-and-spark/parallel_t-SNE-assembly-0.1.0-SNAPSHOT.jar
 gsutil cp \
- target/scala-2.12/parallel_t-sne_2.12-"$(yq ".version" src/main/resources/config.yaml)".jar \
- gs://"$(getYamlValue gsBucket)"/parallel_t-sne_2.12-"$(yq ".version" src/main/resources/config.yaml)".jar
+ target/scala-2.12/parallel_t-sne_2.12-"$(getYamlValue .version)".jar \
+ gs://"$(getYamlValue .shellConfig.gsBucket)"/parallel_t-sne_2.12-"$(getYamlValue .version)".jar
 
 # if shellConfig.createCluster is true, create a new cluster
-if [ "$(getYamlValue createCluster)" = "true" ]; then
+if [ "$(getYamlValue .shellConfig.createCluster)" = "true" ]; then
   # Create the cluster
-  gcloud dataproc clusters create "$(getYamlValue clusterName)" \
+  gcloud dataproc clusters create "$(getYamlValue .shellConfig.clusterName)" \
     --region=us-central1 \
     --zone= \
-    --master-machine-type="$(getYamlValue masterMachineType)" \
+    --master-machine-type="$(getYamlValue .shellConfig.masterMachineType)" \
     --master-boot-disk-size=100 \
     --num-workers=2 \
-    --worker-machine-type="$(getYamlValue workerMachineType)" \
+    --worker-machine-type="$(getYamlValue .shellConfig.workerMachineType)" \
     --worker-boot-disk-size=100 \
-    --image-version="$(getYamlValue imageVersion)" \
-    --bucket="$(getYamlValue gsBucket)"
+    --image-version="$(getYamlValue .shellConfig.imageVersion)" \
+    --bucket="$(getYamlValue .shellConfig.gsBucket)"
 else # else, start the existing cluster
   # Start the cluster
-  gcloud dataproc clusters start "$(getYamlValue clusterName)" \
+  gcloud dataproc clusters start "$(getYamlValue .shellConfig.clusterName)" \
     --region=us-central1
 fi
 
 # Run the job
 gcloud dataproc jobs submit spark \
-  --cluster="$(getYamlValue clusterName)" \
+  --cluster="$(getYamlValue .shellConfig.clusterName)" \
   --region=us-central1 \
   --class=Main \
-  --jars=gs://scala-and-spark/parallel_t-sne_2.12-"$(yq ".version" src/main/resources/config.yaml)".jar
+  --jars=gs://scala-and-spark/parallel_t-sne_2.12-"$(getYamlValue .version)".jar
   #  --jars=gs://scala-and-spark/parallel_t-SNE-assembly-0.1.0-SNAPSHOT.jar \
 
 # If shellConfig.deleteCluster is false, stop the cluster
-if [ "$(getYamlValue deleteCluster)" = "false" ]; then
-  # Stop the cluster
-  gcloud dataproc clusters stop "$(getYamlValue clusterName)" \
-    --region=us-central1
-else # else, delete the cluster
-  # Delete cluster
-  gcloud dataproc clusters delete "$(getYamlValue clusterName)" \
-    --region=us-central1
+#if [ "$(getYamlValue .shellConfig.deleteCluster)" = "false" ]; then
+#  # Stop the cluster
+#  gcloud dataproc clusters stop "$(getYamlValue .shellConfig.clusterName)" \
+#    --region=us-central1
+#else # else, delete the cluster
+#  # Delete cluster
+#  gcloud dataproc clusters delete "$(getYamlValue .shellConfig.clusterName)" \
+#    --region=us-central1
+#
+#fi
 
-fi
+# Create dir for data
+mkdir "data/$(getYamlValue .version)"
 
 # Copy Dataproc Job output to local project folder data
-gsutil cp -r "gs://$(getYamlValue gsBucket)/export/" "data/$(getYamlValue version)"
+gsutil cp -r "gs://$(getYamlValue .shellConfig.gsBucket)/export/" "data/$(getYamlValue .version)/"
 
 # Copy config.yaml to local project folder data
-cp src/main/resources/config.yaml "data/$(getYamlValue version)"
+cp src/main/resources/config.yaml "data/$(getYamlValue .version)/"
 
 # If shellConfig.empyBucket is true, empty the bucket
-if [ "$(getYamlValue emptyGSBucket)" = "true" ]; then
+if [ "$(getYamlValue .shellConfig.emptyGSBucket)" = "true" ]; then
   # Empty gs bucket
-  gsutil rm -r "gs://$(getYamlValue gsBucket)/export/"
+  gsutil rm -r "gs://$(getYamlValue .shellConfig.gsBucket)/export/"
 fi
 
 # Run R file to visualize t-SNE
